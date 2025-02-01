7ae9eb8d60dd969b6665cbc6299816c8f48aa406
Devisacoder <devisa.coder@gmail.com>
2025-01-31 19:40:58 -0500

    cambios en estilo de letra, tamanio y espaciado, color de fondo y de letras

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -9,13 +9,15 @@ export default {
 				grisClaro: '#B6B6B6',
 				grisOscuro: '#B6B6B6',
 				yellow_two:"#fcaf3c",
-				custom_yellow:"#f9a34f"
+				custom_yellow:"#f9a34f",
+				grayBackground: "#D9D9D9"
 			},  
 			fontFamily: {
 				sans: ['Roboto', 'sans-serif'], 
 				work: ['Work Sans', 'sans-serif'], 
 				sawarabi: ['Sawarabi Mincho', 'serif'],
-				raleway: ['Releway', 'serif']
+				raleway: ['Releway', 'serif'],
+				robotoSlab: ['Roboto Slab', 'serif']
 			},
 			boxShadow: {
 				custom: '0 4px 10px rgba(0, 0, 0, 0.20)',
