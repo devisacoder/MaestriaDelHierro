dce5e59a9759c9eddde5cafc1b924a5e3b843aed
Devisacoder <devisa.coder@gmail.com>
2025-02-14 19:58:11 -0500

    mejoras de css, como tamanios en letras y tamanios de iconos, espaciados en logos

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel'
-import { image } from '@astrojs/image';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
-  integrations: [tailwind()], [image()]
+  integrations: [tailwind()]
 });
